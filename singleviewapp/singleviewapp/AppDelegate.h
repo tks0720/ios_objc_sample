d038a383298980840c7abf01a1cfc99ca7f82763
htks27 <pr.acc.tks0720@gmail.com>
2016-01-02 19:02:20 +0900

    Add SingleViewApplicaiton Project.

---
+++ singleviewapp/singleviewapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  singleviewapp
+//
+//  Created by hrkair on 1/2/16.
+//  Copyright © 2016 hrkair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ singleviewapp/singleviewapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  singleviewapp
+//
+//  Created by hrkair on 1/2/16.
+//  Copyright © 2016 hrkair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
